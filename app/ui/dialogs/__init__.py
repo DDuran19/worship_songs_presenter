0b6a03b927e78eea4e422b8b08859e704b85fee2
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2025-07-12 02:12:19 +0800

    delete

--- app/ui/dialogs/__init__.py
+++
@@ -1,9 +0,0 @@
-from .styled_input_dialog import StyledInputDialog
-from .rename_video_dialog import RenameVideoDialog
-from .settings_dialog import SettingsDialog
-
-__all__ = [
-    'StyledInputDialog',
-    'RenameVideoDialog',
-    'SettingsDialog'
-]
--- app/ui/widgets/__init__.py
+++
@@ -1,5 +0,0 @@
-from .top_bar import TopBar
-from .lyrics_list import LyricsList
-from .video_controls import VideoControls
-
-__all__ = ['TopBar', 'LyricsList', 'VideoControls']
